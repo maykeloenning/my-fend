f0c9b6170b8d6ee5bf5fb9f89d412b4bd5beae02
Maykel Oenning <maykeloenning@Maykels-MacBook-Pro.local>
2021-06-08 16:38:54 +0100

    v.0.1

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,12 +1,31 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+const fetch = require('node-fetch')
+
+dotenv.config();
+
+const urlToAPI = "https://api.meaningcloud.com/sentiment-2.1?key=" + process.env.API_KEY + "&url=" + "user input" + "&lang=en"
 
 const app = express()
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+
+const result = await fetch("https://api.meaningcloud.com/sentiment-2.1?key=" + process.env.API_KEY + "&url=" + "user input" + "&lang=en")
+    try {
+        console.log(result)
+        const response = await result.json();
+        res.send(response)
+        console.log(response)
+    } catch (error) {
+        console.log("error occured ===> ", error);
+
+        
+    }
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -21,3 +40,4 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
