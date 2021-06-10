a99772b851d9cbf693de52e2e6dd03c537378c49
Maykel Oenning <maykeloenning@Maykels-MacBook-Pro.local>
2021-06-10 11:04:51 +0100

    description message

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,5 +1,11 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/base.css'
+import './styles/footer.css'
+import './styles/form.css'
+import './styles/header.css'
+import './styles/resets/css/'
+
 
 console.log(checkForName);
 
