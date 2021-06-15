47a6d3f70e9b83d98578c4c1c267ba33df97527e
Maykel Oenning <maykeloenning@Maykels-MacBook-Pro.local>
2021-06-15 15:49:48 +0100

    “0.12”

--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -19,7 +19,7 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.css$/,
+                test: /\.scss$/,
                 use: ['style-loader', 'css-loader']
             }
         ]
